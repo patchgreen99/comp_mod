d88f803b4abf63cd8b73b4a1edb3ca77f2db874e
Dowdykins <emily.dowd@hotmail.co.uk>
2016-02-23 16:08:05 +0000

    Get and set

--- src/SolarSystem.java
+++ src/SolarSystem.java
@@ -13,10 +13,12 @@ public class SolarSystem {
 /**
  * Object SolarSystem Elements
  */
-private Particle3D[] particle_array;
-private double total_energy; 
-private static double total_initial_momentum;
-private static double com_Velocity;
+private Particle3D[] particleArray;
+private double totalEnergy; 
+private static double totalInitialMomentum;
+private static double comVelocity;
+private int numberTimesteps;
+private double timestep;
 
 /**
  * Constructor for SolarSystem which contructs an array of particles read from a file and adjusts intial velocities
@@ -33,7 +35,7 @@ private static double com_Velocity;
  * @param particle      the particle3D to set it as
  */
 	public void setParticle(int particleIndex, Particle3D particle){
-
+		particleArray[particleIndex] = particle;
 	}
 
 /**
@@ -41,7 +43,7 @@ private static double com_Velocity;
  * @param energy gives a double and is the energy of the particle
  */
 	public void setTotalEnergy(double energy){
-
+		totalEnergy = energy;
 	}
 
 /**
@@ -50,7 +52,7 @@ private static double com_Velocity;
  * @return Particle3D     the particle3D in the particle array with index particleIndex
  */
 	public Particle3D getParticle(int particleIndex){
-		return new Particle3D();
+		return particleArray[particleIndex];	
 	}
 
 /**
@@ -58,7 +60,7 @@ private static double com_Velocity;
  * @return a double of the total energy
  */
 	public double getTotalEnergy(){
-		return 1.0;
+		return totalEnergy;
 	}
 
 /**
@@ -93,7 +95,7 @@ private static double com_Velocity;
  * @param timestep is a double of the updated timestep
  */
 	public void updateVelocity(double timestep){
-
+		
 	}
 
 /**
@@ -148,6 +150,93 @@ private static double com_Velocity;
 	}
 
 
+
+	/**
+	 * Constructor that sets up an array of Particle3D objects taken from ( SUN input file what ever we want ,  can have v = 0)
+	 * SolarSystem class to store data for particles in the system and then simulates the system over time using integration algorithms and writes a trajectory file with the simulation results so that these can be visualised using VMD
+	 * @param  a         object SolarSystem in which the simulation will take place in
+	 * @param  num_steps integer that gives number of steps in integration
+	 * @param  step      double that gives the magnitude of each timestep in the integration
+	 * @return           returns object N_body_simulation
+	 */
+		public N_body_simulation(SolarSystem a , int num_steps, double step){
+
+		}
+
+	/**
+	 * Contructs a simulation that is the gravitational interaction of N bodies by reading the planetary data and simulation constants file.
+	 * @param  sim_file    name of a file in which the simulation parameters are written to once they have been calculated
+	 * @param  planet_File name of a file containing the details of the bodies in the system
+	 * @return             a simulation of the solar system as a set of parameters that can be visualised using VMD
+	 */
+		public N_body_simulation(String sim_file, String planet_File){
+
+		}
+
+	/**
+	 * returns integer number of timesteps set in the simulation is retrieved
+	 * @return integer representing the number of timesteps
+	 */
+		public int getNumberTimesteps(){
+			return 1;
+		}
+
+	/**
+	 * returns the double timestep between each iteration of the integration is retrieved from the simulation
+	 * @return double representing the time step
+	 */
+		public double getTimestep(){
+			return 1.0;
+		}
+
+	/**
+	 * A trajectory file is written that contains the trajectory calculated using the input parameters
+	 * @param filename name of the file created. read from terminal.
+	 */
+		public void outputTrajectoryFile(String filename){
+
+		}
+
+	/**
+	 * file is created to store the calculated fluctuations in energy for each body in the system
+	 * @param filename name of the file created. read from terminal.
+	 */
+		public void outputEnergyFluctuations(String filename){
+
+		}
+
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	/**
+	 * A verlet update in which the velocity and position of each planet is updated per time step
+	 * @param timestep double equal to the timestep of the simulation
+	 */
+
+	    public static void verletUpdate(double timestep){
+		
+		// Update the position using current velocity and force
+		//orbiter.updatePosition(timestep,TimeIntegral.getForce());
+		// Update forces based on new positions
+		//Vector3D force_new = TimeIntegral.getGravity(orbiter,orbitee);
+		
+		// Update the velocity, based on average of current and new force
+		//orbiter.updateVelocity(timestep,Vector3D.addVector3D(TimeIntegral.getForce(), force_new).scalarMul(0.5));
+		
+		// Update the force variable to the new value
+		//TimeIntegral.setForce(force_new);
+	    }
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		
+
+	/**
+	 * Main method. Takes 3 filenames from the terminal as an argument. Planetary Data , Simulation Constants , Output file
+	 * @param  args        file names with particle data
+	 * @throws IOException ensures that program goes into a try/catch block where program can be checked for errors and rectified
+	 */
+		public static void main(String[] args) throws IOException {
+
+		}
+	}
 
 }
 
